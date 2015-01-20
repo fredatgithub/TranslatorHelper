6ba34b1d9765c1f669cb63ddcffcbbb36455690d
Freddy Juhel <domaine75@gmail.com>
2015-01-20 15:40:09 +0100

    Fixing failed unit test ie tohourminutesecond test method

--- UnitTestTranslatorHelper/UnitTest1.cs
+++ UnitTestTranslatorHelper/UnitTest1.cs
@@ -27,9 +27,10 @@ namespace UnitTestTranslatorHelper
     [TestMethod]
     public void TestToHourMinuteSecond()
     {
-      Assert.AreEqual("00h:00m:19s:000ms", FormMain.ToHourMinuteSecond(19));
-      Assert.AreEqual("00h:01m:00s:000ms", FormMain.ToHourMinuteSecond(60));
-      Assert.AreEqual("01h:00m:00s:000ms", FormMain.ToHourMinuteSecond(3600));
+      Assert.AreEqual("00h:00m:00s:019ms", FormMain.ToHourMinuteSecond(19));
+      Assert.AreEqual("00h:00m:01s:000ms", FormMain.ToHourMinuteSecond(1000));
+      Assert.AreEqual("00h:01m:00s:000ms", FormMain.ToHourMinuteSecond(60000));
+      Assert.AreEqual("01h:00m:00s:000ms", FormMain.ToHourMinuteSecond(3600000));
       Assert.AreEqual("00h:00m:00s:000ms", FormMain.ToHourMinuteSecond(0));
     }
 
